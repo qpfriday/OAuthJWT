4b1dadba8772314b4577e2e581333e08a9614be6
chan <qpfriday@gmail.>
2024-04-24 20:55:26 +0900

    Add: OAuth2UserService 완료

--- src/main/java/org/study/oauthjwt/service/CustomOAuth2UserService.java
+++ src/main/java/org/study/oauthjwt/service/CustomOAuth2UserService.java
@@ -5,9 +5,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import org.study.oauthjwt.dto.GoogleResponse;
-import org.study.oauthjwt.dto.NaverResponse;
-import org.study.oauthjwt.dto.OAuth2Response;
+import org.study.oauthjwt.dto.*;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -28,5 +26,13 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         } else {
             return null;
         }
+        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername(username);
+        userDTO.setName(oAuth2Response.getName());
+        userDTO.setRole("ROLE_USER");
+
+        return new CustomOAuth2User(userDTO);
     }
 }
