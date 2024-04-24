07440370f43f6271748cbe69aac0de21318de7cb
chan <qpfriday@gmail.>
2024-04-24 19:49:21 +0900

    first commit

---
+++ src/main/java/org/study/oauthjwt/OAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package org.study.oauthjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthJwtApplication.class, args);
+    }
+
+}
