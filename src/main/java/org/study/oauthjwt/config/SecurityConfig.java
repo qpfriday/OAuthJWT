719e9d6bca572ed9d4245a77bdfde7d7186b16b3
chan <qpfriday@gmail.>
2024-04-29 19:30:28 +0900

    Add: CORS 설정

--- src/main/java/org/study/oauthjwt/config/SecurityConfig.java
+++ src/main/java/org/study/oauthjwt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.study.oauthjwt.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,9 +8,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.study.oauthjwt.jwt.JWTFilter;
 import org.study.oauthjwt.jwt.JWTUtil;
 import org.study.oauthjwt.oauth2.CustomSuccessHandler;
 import org.study.oauthjwt.service.CustomOAuth2UserService;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity // 컴포넌트 스캔
@@ -29,6 +36,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
         //csrf disable
         http
                 .csrf((auth) -> auth.disable());
@@ -40,6 +67,8 @@ public class SecurityConfig {
         //HTTP Basic 인증 방식 disable
         http
                 .httpBasic((auth) -> auth.disable());
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         //oauth2
         http
